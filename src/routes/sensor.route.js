95ee0e45fc07a1fe189923d2ff44a645bce87645
coding-syntax <iotemailmessager@gmail.com>
2024-10-19 04:44:53 +0100

    Add files via upload

---
+++ src/routes/sensor.route.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllValue,
+  CreateSensor,
+  UpdateSensor,
+  getSingleSensor,
+  deleteSensor,
+  downloadFile,
+} = require("../controllers/sensor.controller.js");
+
+router.get("/", getAllValue);
+router.get("/:id", getSingleSensor);
+router.post("/create", CreateSensor);
+router.post("/update/:id", UpdateSensor);
+router.delete("/delete/:id", deleteSensor);
+router.get("/download/:id", downloadFile);
+module.exports = router;
