97a2d926cfe7c0cc20a0e81ab77654d46bfc52cc
Akatronix <153951875+Akatronix@users.noreply.github.com>
2024-10-31 19:37:01 +0100

    Update SensorBox.jsx

--- frontend/src/components/SensorBox.jsx
+++ frontend/src/components/SensorBox.jsx
@@ -33,7 +33,7 @@ const SensorBox = () => {
             key={index}
             name={value.name}
             info={value.info}
-            date={value.date}
+            mydate={value.date}
           />
         ))}
       </div>
